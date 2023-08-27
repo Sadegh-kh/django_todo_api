f11edc6c3b4b46c7cb12af95e07a50f6495cb99f
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-08-27 17:44:48 +0330

    init collection and resource of todo

--- todos/urls.py
+++ todos/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 
+from . import views
+
 app_name = "todos_api"
-urlpatterns = []
+urlpatterns = [
+    path("<int:pk>/", views.DetailTodoApi.as_view(), name="todo_detail"),
+    path("", views.ListTodoApi.as_view(), name="todo_list"),
+]
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Todo
+from .serializers import TodoSerializers
 
 # Create your views here.
+
+
+class ListTodoApi(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializers
+
+
+class DetailTodoApi(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializers
