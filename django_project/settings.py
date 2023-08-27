667e6391570432c70d6f04c13f7fc9e956fa4f78
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-08-27 18:47:54 +0330

    config setting for CORS and CSRF on SPA architecture

--- django_project/settings.py
+++ django_project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "todos.apps.TodosConfig",
     # 3rd party
     "rest_framework",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -48,6 +49,7 @@ REST_FRAMEWORK = {
     ]
 }
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -56,7 +58,11 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
+CORS_ALLOWED_ORIGINS = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+CSRF_TRUSTED_ORIGINS = ["localhost:3000"]
 ROOT_URLCONF = "django_project.urls"
 
 TEMPLATES = [
